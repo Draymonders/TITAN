4dabe408b5a7a67eaa7df395bd1beeddaa1b3c18
饼干大猪猪 <273655920@qq.com>
2018-05-31 10:18:49 +0800

    修改默认shell脚本

--- titan-manager/src/main/resources/shell/shutdown-agents.sh
+++ titan-manager/src/main/resources/shell/shutdown-agents.sh
@@ -4,15 +4,13 @@
 AGENT_NUM=2
 
 #所有agent机器地址
-AGENT_ADDRESS=('192.168.1.1'
-'192.168.1.2'
-)
+AGENT_ADDRESS=('10.180.4.231' '10.180.4.232' '10.180.4.233' '10.180.4.234')
 
 #停止所有机器上的agents节点
 for address in ${AGENT_ADDRESS[*]}
 do
-        nohup ssh root@$address "cd /usr/local/yunji/titan-agent*; sh shutdown.sh &" >/dev/null 2>log &
+        nohup ssh root@$address "cd /root/titan/titan-agent*; sh shutdown.sh &" >/dev/null 2>log &
 done
-sleep 5 
+sleep 5
 echo '所有机器上的agent节点已经全部停止'
 
